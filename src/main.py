f1389305ef5315372ab14627be8397a6de54ab4c
Theo <theo@landegger.com>
2026-08-25 12:49:12 -0400

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import sys
+
+class Lisp:
+    @staticmethod
+    def parse(s: str) -> int:
+        stack = []
+        num = ""
+        for c in s:
+            if c in "1234567890":
+                num += c
+            elif num:
+                stack.append(int(num))
+                num = ""
+            if c in "+-":
+                stack.append(c)
+            elif c == ")":
+                print(stack)
+                nums = []
+                while stack[-1] not in ["+","-"]:
+                    nums.append(stack.pop())
+                opp = stack.pop()
+                if opp == "+":
+                    stack.append(sum(nums))
+                if opp == "-":
+                    nums = [n * -1 for n in nums]
+                    nums[-1] *= -1
+                    stack.append(sum(nums))
+        return stack.pop()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(Lisp().parse(sys.argv[1]))
+
+
---
+++ tests/test_main.py
@@ -0,0 +1,41 @@
+import unittest
+
+from src.main import Lisp
+
+
+class TestLisp(unittest.TestCase):
+
+    def test_flat(self):
+        self.assertEqual(Lisp.parse("(+ 1 2)"), 3)
+        self.assertEqual(Lisp.parse("(- 10 4)"), 6)
+        self.assertEqual(Lisp.parse("(+ 0 0)"), 0)
+        self.assertEqual(Lisp.parse("(- 3 7)"), -4)
+
+    # def test_negative_literals(self):
+    #     self.assertEqual(Lisp.parse("(+ -5 5)"), 0)
+    #     self.assertEqual(Lisp.parse("(- -5 -3)"), -2)
+    #     self.assertEqual(Lisp.parse("(+ -2 -3)"), -5)
+
+    def test_single_nesting(self):
+        self.assertEqual(Lisp.parse("(+ (+ 1 2) 3)"), 6)
+        self.assertEqual(Lisp.parse("(+ 1 (+ 2 3))"), 6)
+        self.assertEqual(Lisp.parse("(- (- 9 4) 2)"), 3)
+        self.assertEqual(Lisp.parse("(- 100 (+ 20 30))"), 50)
+
+    def test_both_args_nested(self):
+        self.assertEqual(Lisp.parse("(+ (- 8 3) (- 10 6))"), 9)
+        self.assertEqual(Lisp.parse("(- (+ 5 5) (+ 2 2))"), 6)
+
+    def test_chains(self):
+        self.assertEqual(Lisp.parse("(+ (+ (+ 1 1) 1) 1)"), 4)
+        self.assertEqual(Lisp.parse("(- 20 (- 10 (- 5 2)))"), 13)
+
+    def test_deep_mixed(self):
+        self.assertEqual(Lisp.parse("(+ (- (+ 2 3) (- 8 4)) (+ (- 6 1) (+ 0 2)))"), 8)
+        self.assertEqual(Lisp.parse("(- (+ (- 1 2) (+ 3 4)) (- (+ 5 6) (- 7 8)))"), -6)
+
+    def test_large_ints(self):
+        self.assertEqual(Lisp.parse("(+ 999999999999 1)"), 1000000000000)
+
+if __name__ == '__main__':
+    unittest.main()
